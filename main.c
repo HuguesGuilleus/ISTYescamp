986aaa1f840f4fc916f79b8e18afec42da1e6a41
Maxime Vincent <mail@maximevincent.fr>
2019-09-18 12:19:22 +0200

    Ajout fichier prof

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "graphics.h"
+#include "main.h"
+
+int main(){
+	init_graphics(LARGEUR,HAUTEUR);
+	
+	
+	
+	
+	wait_escape();
+	return(0);
+}
+
+int init_plateau(int largeur,int hauteur){
+	
+	
+}
+
+
+
+
+
+
+
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+	#define MAIN_H 
+	
+	#define HAUTEUR 800
+	#define LARGEUR 800
+
+	int init_plateau(int largeur,int hauteur);
+
+#endif
