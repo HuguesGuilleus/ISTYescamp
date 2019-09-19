5a3cb96a079e6dd293313d5f324d39e1ec7a9b38
Maxime Vincent <mail@maximevincent.fr>
2019-09-19 04:51:17 +0200

    mouv

---
+++ controleur/main.h
@@ -0,0 +1,11 @@
+//Maxime VINCENT, Hugues GUILLEUS
+
+#ifndef MAIN_H
+	#define MAIN_H
+
+	#include "../vue/graphics.h"
+	#include "../controleur/partie.h"
+	#include "../modele/modele.h"
+
+
+#endif
