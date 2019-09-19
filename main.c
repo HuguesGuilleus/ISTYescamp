baf5b9b93c99820b76d60fc448778aa142a388e4
Maxime Vincent <mail@maximevincent.fr>
2019-09-19 03:20:34 +0200

    main.c fic

--- main.c
+++ main.c
@@ -6,13 +6,19 @@ int main(){
 
 	init_graphics(L_FENETRE,H_FENETRE);
 	affiche_auto_off();
-
+	
 	init_piece1_debug();
+	if (ig == 2)
+		init_piece2_debug();
+		
 	afficher_panneau_info();
 	afficher_panneau_jeu(ig);
 
 	while (TRUE) {
-		boiteOrigine = attend_click_numbox(ig);
+		do{
+			boiteOrigine = attend_click_numbox(ig);
+		}while(est_numbox_vide(boiteOrigine));
+		
 		boiteDest    = attend_click_numbox(ig);
 		deplacement_simple(boiteOrigine, boiteDest);
 		afficher_plateau(ig);
