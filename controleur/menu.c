569ad0957af6d756f77af6fcf906c90eaba4cf1f
hugues <ghugues@netc.fr>
2019-09-25 17:23:06 +0200

    style controleur/menu.c

--- controleur/menu.c
+++ controleur/menu.c
@@ -1,22 +1,21 @@
+// Maxime VINCENT, Hugues GUILLEUS
+
 #include "menu.h"
 
-/***** MENU *****/
-
-BOOL est_dans_bouton_quitter(POINT clic){
-	return (clic.y >= 50 && clic.y <= 150 && clic.x <= (L_FENETRE-50) && clic.x >= 50);
+BOOL est_dans_bouton_quitter(POINT clic) {
+	return clic.y >= 50 && clic.y <= 150 && clic.x <= (L_FENETRE-50) && clic.x >= 50 ;
 }
 
-BOOL est_dans_bouton_JvsJ(POINT clic){
-	return (clic.y >= 350 && clic.y <= 450 && clic.x <= (L_FENETRE-50) && clic.x >= 50);
+BOOL est_dans_bouton_JvsJ(POINT clic) {
+	return clic.y >= 350 && clic.y <= 450 && clic.x <= (L_FENETRE-50) && clic.x >= 50 ;
 }
 
-BOOL est_dans_bouton_JvsIA(POINT clic){
-	return (clic.y >= 200 && clic.y <= 300 && clic.x <= (L_FENETRE-50) && clic.x >= 50);
-
+BOOL est_dans_bouton_JvsIA(POINT clic) {
+	return clic.y >= 200 && clic.y <= 300 && clic.x <= (L_FENETRE-50) && clic.x >= 50 ;
 }
 
-BOOL est_dans_bouton_valider(POINT clic){
-	return (clic.y >= 50 && clic.y <= 150 && clic.x <= (L_FENETRE-50) && clic.x >= 50);
+BOOL est_dans_bouton_valider(POINT clic) {
+	return clic.y >= 50 && clic.y <= 150 && clic.x <= (L_FENETRE-50) && clic.x >= 50 ;
 }
 
 BOOL est_dans_bouton_ig_gauche(POINT clic) {
@@ -27,16 +26,15 @@ BOOL est_dans_bouton_ig_droit(POINT clic) {
 	return clic.y >= 350 && clic.y <= 450 && clic.x <= (L_FENETRE-50) && clic.x >= (L_FENETRE-100) ;
 }
 
-BOOL est_dans_bouton_coul(POINT clic){
-	return (clic.y >= 200 && clic.y <= 300 && ( (clic.x <= 100 && clic.x >= 50) || (clic.x <= (L_FENETRE-50) && clic.x >= (L_FENETRE-100))));
+BOOL est_dans_bouton_coul(POINT clic) {
+	return clic.y >= 200 && clic.y <= 300 && ( (clic.x <= 100 && clic.x >= 50) || (clic.x <= (L_FENETRE-50) && clic.x >= (L_FENETRE-100)));
 }
 
-POINT numBoite_to_pointBG_ig_mini(int ig, NUMBOX numboxB){
-	POINT P;
-	int largeur_box = 20;
-	int margeX = (L_FENETRE-(largeur_box*6))/2;
-	int margeY = (H_FENETRE-(largeur_box*6))/2+100;
-
+POINT numBoite_to_pointBG_ig_mini(int ig, NUMBOX numboxB) {
+	POINT P ;
+	int largeur_box = 20 ;
+	int margeX = (L_FENETRE-(largeur_box*6))/2 ;
+	int margeY = (H_FENETRE-(largeur_box*6))/2+100 ;
 
 	switch(ig){
 		case 1:
@@ -57,5 +55,5 @@ POINT numBoite_to_pointBG_ig_mini(int ig, NUMBOX numboxB){
 			break;
 	}
 
-	return P;
+	return P ;
 }
