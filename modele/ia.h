7d513d82e52677568f58dd28504118e346c84bdc
Maxime Vincent <mail@maximevincent.fr>
2019-09-27 16:45:23 +0200

    l'IA peux maintenant !

--- modele/ia.h
+++ modele/ia.h
@@ -12,6 +12,11 @@
 	BOOL lancer_tour_ia(COUL ia, int lisere, int ig);
 	BOOL recup_meilleur_deplacement_ia(NUMBOX* dest, NUMBOX* origine, COUL ia, TYPE caseType, int ig);
 	NUMBOX recup_meilleur_placement_ia(COUL ia,TYPE caseType,int ig);
-	
+	NUMBOX recup_numbox_origine(NUMBOX destination, int nbtabBoxSelect,NUMBOX tabBoxSelect[38] );
+
 	NUMBOX recup_numbox_vide_rand(COUL ia, int ig);
+	
+	NUMBOX* recup_numbox_licorne_en_danger(COUL ia);
+	BOOL va_mourir(NUMBOX licorne,COUL ia);
+
 #endif
